4082099e9d5e05a8870d6c160fdf774fcc05c0bd
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-07-20 14:15:07 -0400

    imported from song-wallet-rails

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+    has_secure_password
+    has_many :user_songs
+    has_many :songs, through: :user_songs
+    has_many :user_friends
+    has_many :friends, through: :user_friends, :class_name => "User", :foreign_key => "friend_id"
+end
