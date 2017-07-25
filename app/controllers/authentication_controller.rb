3383b61990a9aca92e9d582598f29c97337f2ed3
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-07-25 13:12:10 -0400

    Authentication returns user info

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -5,7 +5,8 @@ class AuthenticationController < ApplicationController
         command = AuthenticateUser.call(params[:email], params[:password])
         
         if command.success?
-            render json: { auth_token: command.result }
+            user = User.find_by(email: params[:email])
+            render json: { auth_token: command.result, user: {username:user.username, email:user.email} }
         else
             render json: { error: command.errors }, status: :unauthorized
         end
