637e2fdea947f1ce9d111359a50585a6c05c1ead
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-08-12 17:01:19 -0400

    namespaced under api

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'welcome#home'
+    scope '/api' do
     resources :songs, only: [:create,:update,:show,:index,:destroy]
     resources :users, only: [:create,:update,:show,:index,:destroy]
     resources :setlists, only: [:create,:update,:show,:index,:destroy]
     resources :groups, only: [:create,:update,:show,:index,:destroy]
     
     post 'authenticate', to: 'authentication#authenticate'
-    
+    end
 end
